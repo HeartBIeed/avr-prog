bcf8ff0558269cc80633d7376e58c86c4ecfcec8
Dima <starikov.dmitriy@yandex.ru>
2025-10-03 14:17:39 +0000

    uart

--- uart/uart.c
+++ uart/uart.c
@@ -3,67 +3,77 @@
 
 
 void USART_init(uint16_t speed)
-{
-UBRRH =(speed>>8);	
-UBRRL = speed;
+	{
 
-UCSRB |= (1<<RXEN)|(1<<TXEN)|(1<<RXCIE); //RXCIE - innterr
-UCSRA |= (1<<U2X); // 8 MGc
-UCSRC |= (1<<URSEL)|(1<<USBS)|(1<<UCSZ1)|(1<<UCSZ0); //sel assync umsel non 2 uscz 11 - 8 bit 
-}
+	UBRRH =(speed>>8);	
+	UBRRL = speed;
 
-void USART_TX(unsigned int data)
-{
-	while (!(UCSRA&(1<<UDRE))); //проверяем в усра удре бит что он 0 ибо это готовность к записи
-	
-UDR = data; // sendc data
+	UCSRB |= (1<<RXEN)|(1<<TXEN)|(1<<RXCIE); //RXCIE - innterr
+	UCSRA |= (1<<U2X); // 8 MGc
+	UCSRC |= (1<<URSEL)|(1<<USBS)|(1<<UCSZ1)|(1<<UCSZ0); //sel assync umsel non 2 uscz 11 - 8 bit 
+	}
 
-}
+void USART_TX(unsigned char data)
+	{
+		while (!(UCSRA&(1<<UDRE))); //проверяем UDRE бит что он 0 - это готовность к записи
+		
+		UDR = data; // sendc data
+
+	}
 
 
-volatile unsigned int received_byte = 0;
-volatile unsigned int data_ready = 0;
+volatile uint8_t received_byte = 0;
+volatile uint8_t data_ready = 0;
 
-ISR(USART_RXC_vect) {
-	received_byte = UDR;  // сохраняем байт в глобальную переменную
-	data_ready = 1;
-}
+	ISR(USART_RXC_vect) 
+	{
+		received_byte = UDR;  // сохраняем байт в глобальную переменную
+		data_ready = 1;
+	}
 
-void echo()
-{
-	USART_TX(received_byte);
+void USART_echo()
+	{
+		USART_TX(received_byte);
 
-}
+	}
+
+void USART_ptr_str(char *str) // 
+	{
+		while (*str) 
+		{
+
+		USART_TX(*str++); //
+		
+		}
+
+	}
 
 
 int main(void)
 {
 	
-	iniport();
-	LCD_ini();	
 	
 	sei();
-	USART_init(103);
-	USART_TX('U');
+	USART_init(103); //9600
 
+	USART_TX('O');
+	USART_TX('N');
+
+	unsigned char mystr[10] = {"Hello/0"};
+	USART_ptr_str(mystr);
+
+	unsigned char str_get[6] = {"GET:/0"};
 
     while(1)
     {
-			if (data_ready ==1)
-			{
-				lcd_out(received_byte);
-				echo();
-				data_ready =0;
-			}
+
+		if (data_ready ==1)
+		{
+
+			USART_ptr_str(str_get);
+			USART_echo();
+			data_ready =0;
+		}
 					
     }
 }
-
-
-
-
-
-
-
-
-
