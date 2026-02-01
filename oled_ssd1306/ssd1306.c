b0e47272ab75aa666b5fc52315258dd0675ad199
Дмитрий <starikov.dmitriy@yandex.ru>
2026-02-01 15:34:02 +0300

    ssd1306

--- oled_ssd1306/ssd1306.c
+++ oled_ssd1306/ssd1306.c
@@ -1,36 +1,46 @@
 #include "ssd1306.h"
 
+void ssd1306_cmd(uint8_t cmd)
+{
+    i2c_start(); 
+    i2c_send(0x00);
+    i2c_send(cmd);
+    i2c_stop();
+}
+
+void ssd1306_data(uint8_t data)
+{
+    i2c_start(); 
+    i2c_send(0x40);
+    i2c_send(data);
+    i2c_stop();
+}
 
 static void ssd1306_begin()
     {
-    i2c_start(); 
     i2c_send_adress(0x3c); 
 
-    i2c_send(0xAE); // display off
+    ssd1306_cmd(0xAE); // display off
 
-    i2c_send(0x20);    i2c_send(0x10);
-    i2c_send(0xB0);    i2c_send(0xC8); 
-    i2c_send(0x00);    i2c_send(0x10);
-    i2c_send(0x40);    i2c_send(0x81);
+    ssd1306_cmd(0xD5);    ssd1306_cmd(0x80);
+    ssd1306_cmd(0xA8);    ssd1306_cmd(63); 
+    ssd1306_cmd(0xD3);    ssd1306_cmd(0);
+    ssd1306_cmd(0x40);    ssd1306_cmd(0x8D);
 
-    i2c_send(0xFF);
-    i2c_send(0xA1);
-    i2c_send(0xA6);
+    ssd1306_cmd(0x10);
+    ssd1306_cmd(0x20);
+    ssd1306_cmd(0x00);
 
-    i2c_send(0xA8);    i2c_send(0x3F);
-    i2c_send(0xA4);    i2c_send(0xD3);
-    i2c_send(0x00);    i2c_send(0xD5);
-    i2c_send(0xF0);    i2c_send(0xD9);
+    ssd1306_cmd(0xA0);    ssd1306_cmd(0xC8);
+    ssd1306_cmd(0xDA);    ssd1306_cmd(0x12);
+    ssd1306_cmd(0x81);    ssd1306_cmd(0x9F);
+    ssd1306_cmd(0x22);    ssd1306_cmd(0xD8);
 
-    i2c_send(0x22);    i2c_send(0xDA);
-    i2c_send(0x12);    i2c_send(0xDB);
-    i2c_send(0x20);    i2c_send(0x8D);
+    ssd1306_cmd(0x40);    ssd1306_cmd(0xA4);
+    ssd1306_cmd(0xA6);    ssd1306_cmd(0x2E);
 
+    ssd1306_cmd(0xAF); // display on
 
-    i2c_send(0x14);
-    i2c_send(0xAF); // display on
-
-    i2c_stop();
     }
 
 static void ssd1306_fill(uint8_t b)
@@ -38,21 +48,17 @@ static void ssd1306_fill(uint8_t b)
         i2c_start(); 
         i2c_send_adress(0x3c); 
 
-        i2c_send(0x00);
-
-
-        i2c_send(0x21); i2c_send(0x00); i2c_send(0x7F); 
-        i2c_send(0x22); i2c_send(0x00); i2c_send(0x07);
+        ssd1306_cmd(0x21); 
+        ssd1306_cmd(0x7F); 
+        ssd1306_cmd(0x22);
+        ssd1306_cmd(0x07);
 
         i2c_stop();
 
 
         for (uint16_t i = 0; i < (128*64/8); i++) 
             {
-                i2c_start(); 
-                i2c_send(0x40);
-                i2c_send(b);
-                i2c_stop();
+                ssd1306_data(b);
             }
 
 
